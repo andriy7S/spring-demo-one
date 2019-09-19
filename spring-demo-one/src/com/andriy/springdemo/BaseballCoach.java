aaf2b49771b33593da8ae332d7fee7238b88bc2b
Lapotopo <Lapotopo@DESKTOP-9STL97V>
2019-09-19 03:20:58 -0400

    first

---
+++ spring-demo-one/src/com/andriy/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.andriy.springdemo;
+
+public class BaseballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ spring-demo-one/src/com/andriy/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.andriy.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+}
